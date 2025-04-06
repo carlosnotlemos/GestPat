37ec0e43ad4f26b19a2589a8e02f992ed02f1ad4
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-04-05 21:47:25 -0300

    build(bootstrap): add @popperjs/core
    Add @popperjs/core to the project.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// adicionados pelo importmap
 import "bootstrap"
+import "#popperjs/core"
+//import "jquery"
