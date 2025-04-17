487a6f75df75970f3e3ea2df5393f223d51ba6f4
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-04-16 22:25:21 -0300

    chore(jquery): remove jquery

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "vendor/javascript/bootstrap.bundle.min.js"
-pin "jquery" # @3.7.1
