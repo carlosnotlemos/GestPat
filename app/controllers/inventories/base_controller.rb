06c3d636ab366e5431bcd47f92eddd087190b64d
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-05-12 17:47:13 -0300

    refact(inventories_base): refact using rubocop rules to improve code clarity

--- app/controllers/inventories/base_controller.rb
+++ app/controllers/inventories/base_controller.rb
@@ -1,50 +1,60 @@
+# frozen_string_literal: true
+
 module Inventories
+  # Class Inventories Main
   class BaseController < ApplicationController
     # Identifica quem está acessando essa pagina pela role.
     before_action :check_role
 
     def new
       @inventory = Inventory.new
-      @rooms = Room.all
-
-      if params[:room_id].present?
-        @assets = Asset.where(room_id: params[:room_id])
-      end
+      rooms_load = current_user.rooms.includes(:assets)
+      @rooms = rooms_load
+      @assets = rooms_load.find(params[:room_id]).assets if params[:room_id].present?
     end
 
     def create
-      @inventory = Inventory.new(inventory_params)
-      @inventory.user = current_user
-      @inventory.checked_at = Time.current
+      ActiveRecord::Base.transaction do
+        set_inventory_attributes
 
-      if @inventory.save
-        params[:assets].each do |asset_id, counted_quantity|
-          InventoryAsset.create!(
-            inventory_id: @inventory.id,
-            asset_id: asset_id,
-            counted_quantity: counted_quantity
-          )
+        if @inventory.save
+          create_inventory_assets
+          create_found_itens if params[:found_items].present?
+
+          redirect_to after_create_path, notice: "Inventario criado com sucesso."
+        else
+          render :new, status: :unprocessable_entity
         end
-
-        if params[:found_items].present?
-          params[:found_items].each do |_key, founditem_params|
-            founditem_desc = founditem_params[:description]
-            founditem_quant = founditem_params[:quantity]
-
-            FoundItem.create!(
-              inventory_id: @inventory.id,
-              description: founditem_desc,
-              quantity: founditem_quant
-            )
-          end
-        end
-        redirect_to after_create_path, notice: "Inventario criado com sucesso."
-      else
-        render :new, status: :unprocessable_entity
       end
     end
 
     private
+
+    def set_inventory_attributes
+      @inventory = Inventory.new(inventory_params)
+      @inventory.user = current_user
+      @inventory.checked_at = Time.current
+    end
+
+    def create_found_itens
+      params[:found_items].each_value do |founditem_params|
+        FoundItem.create!(
+          inventory_id: @inventory.id,
+          description: founditem_params[:description],
+          quantity: founditem_params[:quantity]
+        )
+      end
+    end
+
+    def create_inventory_assets
+      params[:assets].each do |asset_id, counted_quantity|
+        InventoryAsset.create!(
+          inventory_id: @inventory.id,
+          asset_id: asset_id,
+          counted_quantity: counted_quantity
+        )
+      end
+    end
 
     def inventory_params
       params.require(:inventory).permit(:room_id, assets: {}, found_items: {})
@@ -56,10 +66,9 @@ module Inventories
 
     # &.role.to_sym , conversão para simbolico, pois excpected_role é simbolica.
     def check_role
-      unless current_user&.role.to_sym == expected_role
-        # WARNING IMPLEMENTATION --> Implementar rota para acesso não autorizado.
-        redirect_to authenticated_root_path, alert: "Acesso não autorizado."
-      end
+      return if current_user&.role&.to_sym == expected_role
+
+      redirect_to authenticated_root_path, alert: "Acesso não autorizado."
     end
   end
 end
